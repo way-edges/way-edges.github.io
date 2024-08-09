18499c2db51ff0c1dc9d4b19d88d1460db6ef904
ogios <ogios@foxmail.com>
2024-08-09 20:27:58 +0800

    hide

--- src/widgets/toc/Toc.tsx
+++ src/widgets/toc/Toc.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { memo, useState } from 'react'
+import { memo, useRef, useState } from 'react'
 import './toc.css'
 import { TocItem, useTocCurrentPos } from './TocState'
 
@@ -23,6 +23,17 @@ const Collapse = memo(({ content, index }: CollapseProps) => {
   const ls = content.children.map((content, i) => <Collapse index={i} content={content} key={i}></Collapse>)
 
   const [is_open, set_open] = useState(false)
+  const list_ref = useRef<HTMLUListElement>(null)
+
+  const list_style = {
+    height: '0px',
+    overflow: 'hidden',
+    transition: 'height 0.3s ease-in-out',
+  }
+
+  if (is_open) {
+    list_style.height = `${list_ref.current!.scrollHeight}px`
+  }
 
   return (
     <li key={index}>
@@ -30,13 +41,19 @@ const Collapse = memo(({ content, index }: CollapseProps) => {
         <a className="content-center flex-1 text-lg" onClick={() => set_pos!(content)}>
           {content.title}
         </a>
-        <div
-          className="flex justify-center items-center rounded-sm w-[30px] collapse-sign"
-          onClick={() => set_open(!is_open)}>
-          <div className="w-[15px] h-[5px] bg-red-300" style={{ rotate: is_open ? '90deg' : '0deg' }}></div>
-        </div>
+
+        {ls.length > 0 ? (
+          <div
+            className="flex justify-center items-center rounded-sm w-[30px] collapse-sign"
+            onClick={() => set_open(!is_open)}>
+            <div className="w-[15px] h-[5px] bg-red-300" style={{ rotate: is_open ? '90deg' : '0deg' }}></div>
+          </div>
+        ) : undefined}
       </div>
-      <ul className="pl-2 collapse-list ">{ls}</ul>
+
+      <ul className="pl-2 collapse-list " ref={list_ref} style={list_style}>
+        {ls}
+      </ul>
     </li>
   )
 })

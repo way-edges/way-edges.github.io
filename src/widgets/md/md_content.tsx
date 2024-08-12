60290c6161306e497b206225ed2aa807fce91bd8
ogios <ogios@foxmail.com>
2024-08-12 12:31:29 +0800

    update doc; add tex support

--- src/widgets/md/md_content.tsx
+++ src/widgets/md/md_content.tsx
@@ -4,6 +4,11 @@ import { memo, useEffect, useState } from 'react'
 import MarkdownPreview from '@uiw/react-markdown-preview'
 import { useTheme } from 'next-themes'
 import Markdown from 'react-markdown'
+import { getCodeString } from 'rehype-rewrite'
+
+import katex from 'katex'
+import 'katex/dist/katex.css'
+import './md.css'
 
 interface MarkdownInfo {
   content: string
@@ -29,6 +34,28 @@ const MarkdownContent = memo(({ content }: MarkdownInfo) => {
         // ignore theme variable type (it's sould only be dark or light)
         wrapperElement={{ 'data-color-mode': theme }}
         style={{ minHeight: '100%', padding: 30, background: 'none' }}
+        components={{
+          code: ({ children = [], className, ...props }) => {
+            if (typeof children === 'string' && /^\$\$(.*)\$\$/.test(children)) {
+              const html = katex.renderToString(children.replace(/^\$\$(.*)\$\$/, '$1'), {
+                throwOnError: false,
+              })
+              return <code dangerouslySetInnerHTML={{ __html: html }} style={{ background: 'transparent' }} />
+            }
+            const code = props.node && props.node.children ? getCodeString(props.node.children) : children
+            if (
+              typeof code === 'string' &&
+              typeof className === 'string' &&
+              /^language-katex/.test(className.toLocaleLowerCase())
+            ) {
+              const html = katex.renderToString(code, {
+                throwOnError: false,
+              })
+              return <code style={{ fontSize: '150%' }} dangerouslySetInnerHTML={{ __html: html }} />
+            }
+            return <code className={String(className)}>{children}</code>
+          },
+        }}
       />
     )
   } else {

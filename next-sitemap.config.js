1aa66fef3aad2ca8de42aa48c213164d4f25dbf8
ogios <ogios@foxmail.com>
2024-08-12 23:12:13 +0800

    update site url

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://example.com',
+  siteUrl: process.env.SITE_URL || 'https://way-edges.github.io ',
   generateRobotsTxt: true, // (optional)
   // ...other options
 }

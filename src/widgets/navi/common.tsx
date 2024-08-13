b09ff1e824f7b940fdbe6528a931cc1cca96b95b
ogios <ogios@foxmail.com>
2024-08-13 09:32:34 +0800

    update to new git repo

--- src/widgets/navi/common.tsx
+++ src/widgets/navi/common.tsx
@@ -41,7 +41,7 @@ MenuDrawer.displayName = 'MenuDrawer'
 export const Contact = memo(() => {
   return (
     <div className="flex flex-col items-center align-center content-center justify-center mr-3">
-      <a href="https://github.com/ogios/way-edges">
+      <a href="https://github.com/way-edges/way-edges">
         <GitHubLogoIcon className="w-[20px] h-[20px] hover:w-[25px] hover:h-[25px] hover:text-[color:var(--link-hover)] hover:translate-x-[2.5px]" />
       </a>
     </div>

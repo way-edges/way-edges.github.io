66b2c145d30a76324b6f51616ca9329874003ba7
ogios <ogios@foxmail.com>
2024-12-09 10:29:37 +0800

    add tray doc

---
+++ src/app/box/tray/page.tsx
@@ -0,0 +1,13 @@
+
+import { get_file_under_data } from '@/lib/local_util'
+import MarkdownContent from '@/widgets/md/md_content'
+import { readFileSync } from 'fs'
+import '@/app/md.css'
+
+export default async function Page() {
+  const path = get_file_under_data('/doc/4.widgets/5.box/2.tray.md')
+  const content = readFileSync(path).toString()
+  return (
+      <MarkdownContent content={content} />
+  )
+}

48f153508c9fb8d34f3f0f25277332aee3921a9f
ogios <ogios@foxmail.com>
2025-01-01 23:03:49 +0800

    update data

--- src/app/widgets/box/page.tsx
+++ src/app/widgets/box/page.tsx
@@ -5,7 +5,7 @@ import { readFileSync } from 'fs'
 import '@/app/md.css'
 
 export default async function Page() {
-  const path = get_file_under_data('/doc/4.widgets/3.box/index.md')
+  const path = get_file_under_data('/doc/4.widgets/2.box/index.md')
   const content = readFileSync(path).toString()
   return (
       <MarkdownContent content={content} />
